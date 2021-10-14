7f6835fb7515d24c9c999c0ce2181a67ac23aad4
RenanGAS <renansakaoki@gmail.com>
2021-10-14 18:47:56 -0300

    chore: add the base for the exercises

---
+++ Activities/Week_03/exs.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void imprimirVetor(int *v, int tam)
+{
+    int i;
+    printf("[");
+    for (i = 0; i < tam; i++)
+    {
+        printf("%d", v[i]);
+        if (i < tam - 1)
+        {
+            printf(",");
+        }
+    }
+    printf("]");
+    printf("\n");
+}
+
+// exercício 01
+
+// exercício 02
+
+// exercício 03
+
+// exercício 04
+
+// exercício 05
+
+// exercício 06
+
+// exercício 07
+
+int main()
+{
+
+    return 0;
+}
