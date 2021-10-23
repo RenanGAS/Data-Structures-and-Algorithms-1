1e4e5cf380ff9f1d95d360643d12b67770a5552b
RenanGAS <renansakaoki@gmail.com>
2021-10-23 16:37:58 -0300

    feat: does the Ex5 and tests it

--- Activities/Week_04/exs.c
+++ Activities/Week_04/exs.c
@@ -121,7 +121,7 @@ void mergeVectors2(int *v1, int tam1, int *v2, int tam2, int *mergedV, int tamMe
     }
 }
 
-// Exercício 4
+// Exercício 4    // Não testado
 
 int numberOfChar1(char str[])
 {
@@ -146,6 +146,28 @@ void numberOfChar2(char str[], int *count)
         {
             *count++;
         }
+    }
+}
+
+// Exercício 5  // Não testado
+
+char *reverseStr1(char str[], int tam)
+{
+    char *inversedStr = (char *)calloc(tam, sizeof(char));
+
+    for (int i = tam - 1; i >= 0; i--)
+    {
+        inversedStr[i] = str[i];
+    }
+
+    return inversedStr;
+}
+
+void reverseStr2(char str[], int tam, char *inversedStr)
+{
+    for (int i = tam - 1; i >= 0; i--)
+    {
+        inversedStr[i] = str[i];
     }
 }
 
@@ -180,13 +202,20 @@ int main()
     mergeVectors2(vet1, 8, vet2, 6, pVet2, 14); // Não testado
     printVector(pVet2, 14);
 
-    printf("\nTeste do exercício 4:\n");
+    printf("\nTeste do exercício 4:\n"); // Não testado
     char str[50] = " Me chamo Renan Guensuke Aoki Sakashita";
     int numChar1 = numberOfChar1(str);
-    printf("\nUsando return: %d\n", numChar1); 
+    printf("\nUsando return: %d\n", numChar1);
     int numChar2 = 0;
     numberOfChar2(str, &numChar2);
     printf("\nUsando a estratégia do scanf: %d\n", numChar2);
-    
+
+    printf("\nTeste do exercício 5:\n"); // Não testado
+    char str[10] = "UTFPR - Campo Mourao";
+    // Dúvida na aplicação do return em strings
+    char reverseStr[10] = "";
+    reverseStr2(str, 10, reverseStr);
+    printf("\n=> Usando a estratégia do scanf: %s\n", reverseStr);
+
     return 0;
 }
