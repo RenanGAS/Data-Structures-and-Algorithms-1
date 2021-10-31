6008964f7cb2001c02579ddd5f9f62f6f19815bc
RenanGAS <renansakaoki@gmail.com>
2021-10-31 08:38:40 -0300

    feat: create tests for the functions' prototype

--- Activities/Week_05/main.c
+++ Activities/Week_05/main.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <assert.h>
+
 // Passo 1.1 - Abstração dos dados
 /*
 - Tempo Inicial;
@@ -29,3 +32,46 @@ void start_timer(Timer *ptr_timer);
 void stop_timer(Timer *ptr_timer);
 void reset_timer(Timer *ptr_timer);
 float result_timer(Timer *ptr_timer);
+
+// Passo 2.1 - Criar testes para as funções
+
+int main()
+{
+    printf("\nTESTE INICIALIZADO:\n\n");
+
+    printf("- Testando create_timer\n");
+    Timer *timer = create_timer();
+    assert(timer != NULL);
+
+    printf("- Testando start_timer\n");
+    start_timer(timer);
+    assert(timer->begin != 0);
+
+    long int end = 1000*1000*1000;
+    for (int i = 0; i < end; i++)
+    {
+    }
+
+    printf("- Testando stop_timer\n");
+    stop_timer(timer);
+    assert(timer->end != 0);
+    assert(timer->end != timer->begin);
+
+    printf("- Testando result_timer\n");
+    float result = result_timer(timer);
+    assert(result != 0);
+
+    printf("- Testando reset_timer\n");
+    reset_timer(timer);
+    assert(timer->begin == 0);
+    assert(timer->end == 0);
+
+    printf("- Testando free_timer\n");
+    Timer *ptr_test = timer;
+    free_timer(&timer);
+    assert(timer != ptr_test);
+
+    printf("\nTESTE FINALIZADO!\n");
+    
+    return 0;
+}
