5c2cfbef15a7941046528f53bb6ac8edc40c760b
RenanGAS <renansakaoki@gmail.com>
2021-11-06 17:15:37 -0300

    feat: do some of the function of vector_TAD

--- Activities/Week_06/vector_TAD.c
+++ Activities/Week_06/vector_TAD.c
@@ -1,4 +1,5 @@
 #include "vector_TAD.h"
+#include <stdlib.h>
 
 struct vector
 {
@@ -6,3 +7,63 @@ struct vector
     int tam;
     int qtd;
 };
+
+Vector *create_vector()
+{
+    Vector *newVector = (Vector *)calloc(4, sizeof(int));
+    return newVector;
+}
+
+int length_vector(Vector *v)
+{
+    for (size_t i = 0; i < v->tam; i++)
+    {
+        /* code */
+    }
+    
+}
+
+void free_vector(Vector **endVector)
+{
+    free(*endVector);
+    *endVector = NULL;
+}
+
+bool addIn_vector(Vector *v, int elemento)
+{
+    int lenVector = length_vector(v);
+    if (v->vet[lenVector - 1] == 0)
+    {
+        v->vet[lenVector - 1] = elemento;
+        return true;
+    }
+    else
+    {
+        free_vector(&v);
+        v = (Vector *)calloc(2, lenVector);
+        return false;
+    }
+}
+
+bool putIn_vector(Vector *v, int elemento, int posicao)
+{
+    int lenVector = length_vector(v);
+    if (posicao < lenVector)
+    {
+        v->vet[posicao] = elemento;
+        return true;
+    }
+    else
+    {
+        free_vector(&v);
+        v = (Vector *)calloc(2, lenVector);
+        return false;
+    }
+}
+
+bool replaceIn_vector(Vector *v, int posicao, int novoElemento)
+{
+    v->vet[posicao] = novoElemento;
+
+    return true;    
+}
--- Activities/Week_06/vector_TAD.h
+++ Activities/Week_06/vector_TAD.h
@@ -6,17 +6,17 @@
 
 typedef struct vector Vector;
 
-Vector* create_vector();
-bool addIn_vector(Vector* v, int elemento);
-bool putIn_vector(Vector* v, int elemento, int posicao);
-bool replaceIn_vector(Vector* v, int posicao, int novoElemento);
-bool rmPosition_vector(Vector* v, int posicao, int* endereco);
-int rmElement_vector(Vector* v, int elemento);
-int lenght_vector(Vector* v);
-bool elementIn_vector(Vector* v, int posicao, int* saida);
-int positionIn_vector(Vector* v, int elemento);
-void print_vector(Vector* v);
-void free_vector(Vector** endVetor);
-bool toString_vector(Vector* v, char* saida);
+Vector *create_vector();
+bool addIn_vector(Vector *v, int elemento);
+bool putIn_vector(Vector *v, int elemento, int posicao);
+bool replaceIn_vector(Vector *v, int posicao, int novoElemento);
+bool rmPosition_vector(Vector *v, int posicao, int *endereco);
+int rmElement_vector(Vector *v, int elemento);
+int lenght_vector(Vector *v);
+bool elementIn_vector(Vector *v, int posicao, int *saida);
+int positionIn_vector(Vector *v, int elemento);
+void print_vector(Vector *v);
+void free_vector(Vector **endVetor);
+bool toString_vector(Vector *v, char *saida);
 
 #endif
