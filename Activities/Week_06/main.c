41c26f67f10eabb63033c2494f624c0c2c6656ae
RenanGAS <renansakaoki@gmail.com>
2021-11-06 15:09:01 -0300

    fix: rename functions

--- Activities/Week_06/main.c
+++ Activities/Week_06/main.c
@@ -3,31 +3,31 @@
 #include "vector_TAD.h"
 
 int main(){
-	Vector* v = vet_criar();
+	Vector* v = create_vector();
 
-	vet_inserir(v, 10);
-	vet_inserir(v, 20);
-	vet_inserir(v, 30);
-	vet_imprimir(v); // [10,20,30]
+	putIn_vector(v, 10, 0);
+	putIn_vector(v, 20, 1);
+	putIn_vector(v, 30, 2);
+	print_vector(v); // [10,20,30]
 	
-	vet_inserir(v, 15, 1);
-	vet_imprimir(v); // [10,15,20,30]
+	putIn_vector(v, 15, 1);
+	print_vector(v); // [10,15,20,30]
 	
-	vet_inserir(v, 5, 0);
-	vet_imprimir(v); // [5,10,15,20,30]
+	putIn_vector(v, 5, 0);
+	print_vector(v); // [5,10,15,20,30]
 	
 	int elemento;
-	if (vet_removerPosicao(v, 1, &elemento)){
+	if (rmPosition_vector(v, 1, &elemento)){
 	    printf("O elemento %d foi removido\n", elemento); // O elemento 10 foi removido
-	    vet_imprimir(v); // [5,15,20,30]
+	    print_vector(v); // [5,15,20,30]
 	}
 	
 	// if (vet_removerPosicao(v, 10, &elemento) == false){
-	if (!vet_removerPosicao(v, 10, &elemento)){
+	if (!rmPosition_vector(v, 10, &elemento)){
 	    printf("Remocao invalida\n");
 	} 
 	
 	char str[200];
-	vet_toString(v, str);
+	toString_vector(v, str);
 	printf("%s", str); //[5,15,20,30]
 }
