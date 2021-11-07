497e4486903a0e658ae8d379792349956ecbab04
RenanGAS <renansakaoki@gmail.com>
2021-11-07 12:59:18 -0300

    feat: do create and addIn functions of Vector TAD

--- Activities/Week_06/vector_TAD.c
+++ Activities/Week_06/vector_TAD.c
@@ -8,62 +8,98 @@ struct vector
     int qtd;
 };
 
+void double_vector(Vector *v, Vector **end_v)
+{
+    int tam_origin = v->tam;
+
+    Vector *doubleVector = (Vector *)calloc(tam_origin * 2, sizeof(int));
+    doubleVector->tam = tam_origin * 2;
+    doubleVector->qtd = v->qtd;
+
+    for (int i = 0; i < tam_origin; i++)
+    {
+        doubleVector->vet[i] = v->vet[i];
+    }
+
+    free(*end_v);
+    *end_v = doubleVector;
+}
+
+bool validate_vector(Vector *v)
+{
+    int t = v->tam;
+    int q = v->qtd;
+
+    if (t == q)
+    {
+        double_vector(v, &v);
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
 Vector *create_vector()
 {
-    Vector *newVector = (Vector *)calloc(4, sizeof(int));
+    Vector *newVector = (Vector *)calloc(10, sizeof(int));
+    newVector->tam = 10;
+    newVector->qtd = 0;
+
     return newVector;
-}
-
-int length_vector(Vector *v)
-{
-    for (size_t i = 0; i < v->tam; i++)
-    {
-        /* code */
-    }
-    
-}
-
-void free_vector(Vector **endVector)
-{
-    free(*endVector);
-    *endVector = NULL;
 }
 
 bool addIn_vector(Vector *v, int elemento)
 {
-    int lenVector = length_vector(v);
-    if (v->vet[lenVector - 1] == 0)
+    if (validate_vector(v))
     {
-        v->vet[lenVector - 1] = elemento;
+        int q = v->qtd;
+        v->vet[q] = elemento;
         return true;
     }
     else
     {
-        free_vector(&v);
-        v = (Vector *)calloc(2, lenVector);
         return false;
     }
 }
 
 bool putIn_vector(Vector *v, int elemento, int posicao)
 {
-    int lenVector = length_vector(v);
-    if (posicao < lenVector)
-    {
-        v->vet[posicao] = elemento;
-        return true;
-    }
-    else
-    {
-        free_vector(&v);
-        v = (Vector *)calloc(2, lenVector);
-        return false;
-    }
 }
 
 bool replaceIn_vector(Vector *v, int posicao, int novoElemento)
 {
-    v->vet[posicao] = novoElemento;
+}
 
-    return true;    
+bool rmPosition_vector(Vector *v, int posicao, int *endereco)
+{
+}
+
+int rmElement_vector(Vector *v, int elemento)
+{
+}
+
+int lenght_vector(Vector *v)
+{
+}
+
+bool elementIn_vector(Vector *v, int posicao, int *saida)
+{
+}
+
+int positionIn_vector(Vector *v, int elemento)
+{
+}
+
+void print_vector(Vector *v)
+{
+}
+
+void free_vector(Vector **endVetor)
+{
+}
+
+bool toString_vector(Vector *v, char *saida)
+{
 }
