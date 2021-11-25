a30c899536457a77680e5fc67ab9894ff41c5bfb
RenanGAS <renansakaoki@gmail.com>
2021-11-25 19:59:24 -0300

    test: test the create, addTo and toString functions of TAD Pilha

--- Activities/Week_08/main.c
+++ Activities/Week_08/main.c
@@ -6,44 +6,52 @@
 
 int main()
 {
-
-    Pilha *p1 = pilha_criar();
-    pilha_empilhar(p1, 10);
-    pilha_empilhar(p1, 20);
-    pilha_empilhar(p1, 30);
+    printf("\nTESTE PILHA_CRIAR || PILHA_EMPILHAR || PILHA_TOSTRING\n");
+    Pilha *new_stack1 = pilha_criar();
+    pilha_empilhar(new_stack1, 65);
+    pilha_empilhar(new_stack1, 66);
+    pilha_empilhar(new_stack1, 67);
+    pilha_empilhar(new_stack1, 68);
+    pilha_empilhar(new_stack1, 69);
+    pilha_empilhar(new_stack1, 70);
+    pilha_empilhar(new_stack1, 71);
+    pilha_empilhar(new_stack1, 72);
+    pilha_empilhar(new_stack1, 73);
+    pilha_empilhar(new_stack1, 74);
 
     char str[200];
-    pilha_toString(p1, str);
-    printf("%s\n", str); //[10,20,30]
+    pilha_toString(new_stack1, str);
+    printf("\nRESULTADO:\n");
+    printf("\n%s\n", str);
 
-    int elemento;
+    // int element;
 
-    pilha_topo(p1, &elemento);
-    printf("Topo: %d\n", elemento); // 30
+    // pilha_topo(new_stack1, &element);
+    // printf("Topo: %d\n", element); 
 
-    while (!pilha_vazia(p1))
-    { // 30 20 10
-        pilha_desempilhar(p1, &elemento);
-        printf("Desempilhado: %d\n", elemento);
-    }
+    // while (!pilha_vazia(new_stack1))
+    // {
+    //     pilha_desempilhar(new_stack1, &element);
+    //     printf("Desempilhado: %d\n", element);
+    // }
 
-    /***********************************************
-    * OPERAÇÕES NA PILHA VAZIA
-    ************************************************/
-    if (!pilha_desempilhar(p1, &elemento))
-    {
-        printf("Pilha vazia");
-    }
+    // /***********************************************
+    // * OPERAÇÕES NA PILHA VAZIA
+    // ************************************************/
+    // if (!pilha_desempilhar(new_stack1, &element))
+    // {
+    //     printf("Pilha vazia");
+    // }
 
-    if (!pilha_topo(p1, &elemento))
-    {
-        printf("Pilha vazia");
-    }
+    // if (!pilha_topo(new_stack1, &element))
+    // {
+    //     printf("Pilha vazia");
+    // }
 
-    pilha_toString(p1, str);
-    printf("%s\n", str); //[]
+    // pilha_toString(new_stack1, str);
+    // printf("%s\n", str); //[]
 
-    pilha_destruir(&p1);
+    // pilha_destruir(&new_stack1);
 
     return 0;
 }
