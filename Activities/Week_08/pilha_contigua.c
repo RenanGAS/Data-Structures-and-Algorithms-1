4f62a9e5c6b2b1a9d6f17730fd235fbf7829e1ab
RenanGAS <renansakaoki@gmail.com>
2021-11-25 19:56:43 -0300

    feat: do pilha_criar, pilha_empilhar and pilha_toString functions

--- Activities/Week_08/pilha_contigua.c
+++ Activities/Week_08/pilha_contigua.c
@@ -2,9 +2,6 @@
 
 #define TAM 10
 
-/**************************************
-* Especificação do dados
-**************************************/
 struct pilha
 {
     int qtde;
@@ -12,36 +9,74 @@ struct pilha
     TipoElemento *vetor;
 };
 
-/**************************************
-* Funções auxiliares
-**************************************/
-void verifica_aumenta(Pilha *p);
-void verifica_diminui(Pilha *p);
+// Funções Auxiliares
 
-/**************************************
-* Implementação das funções
-**************************************/
-Pilha *pilha_criar()
+void freeAtt_vector(TipoElemento **vector, TipoElemento *newVector)
 {
-    Pilha *p = (Pilha *)malloc(sizeof(Pilha));
-    p->vetor = (TipoElemento *)calloc(TAM, sizeof(TipoElemento));
-    p->tam = TAM;
-    p->qtde = 0;
-    return p;
+    free(*vector);
+    *vector = NULL;
+    *vector = newVector;
 }
 
-void pilha_destruir(Pilha **endereco);
-
-bool pilha_empilhar(Pilha *p, TipoElemento elemento)
+void verifica_aumenta(Pilha *stack)
 {
-    verifica_aumenta(p);
+    if (stack->qtde == stack->tam)
+    {
+        int *new_vector = (int *)malloc(stack->tam * 2 * sizeof(TipoElemento));
 
-    p->vetor[p->qtde] = elemento;
-    p->qtde++;
+        for (int i = 0; i < stack->qtde; i++)
+        {
+            new_vector[i] = stack->vetor[i];
+        }
+
+        freeAtt_vector(&stack->vetor, new_vector);
+
+        printf("\nATUALIZACAO: VETOR DA PILHA DUPLICADO\n");
+    }
+}
+
+void verifica_diminui(Pilha *p);
+
+// Funções Principais
+
+Pilha *pilha_criar()
+{
+    Pilha *new_stack = (Pilha *)malloc(sizeof(Pilha));
+    new_stack->vetor = (TipoElemento *)calloc(TAM, sizeof(TipoElemento));
+    new_stack->tam = TAM;
+    new_stack->qtde = 0;
+    return new_stack;
+}
+
+bool pilha_empilhar(Pilha *stack, TipoElemento element)
+{
+    verifica_aumenta(stack);
+
+    stack->vetor[stack->qtde] = element;
+    stack->qtde++;
     return true;
 }
 
 bool pilha_desempilhar(Pilha *p, TipoElemento *saida);
 bool pilha_topo(Pilha *p, TipoElemento *saida);
+void pilha_destruir(Pilha **endereco);
 bool pilha_vazia(Pilha *p);
-void pilha_toString(Pilha *p, char *saida);
+
+void pilha_toString(Pilha *stack, char *string)
+{
+    strcat(string, "[");
+
+    for (int i = 0; i < stack->qtde; i++)
+    {
+        char element[20];
+        sprintf(element, "%d", stack->vetor[i]);
+        strcat(string, element);
+
+        if (i < stack->qtde - 1)
+        {
+            strcat(string, ", ");
+        }
+    }
+
+    strcat(string, "]");
+}
